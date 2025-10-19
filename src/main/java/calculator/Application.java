8033360ded85a02289302c3bc2f000ff59626f00
tot0ma <dstdbsgml@naver.com>
2025-10-19 11:51:44 +0900

    feat: 구분자로 입력을 파싱하는 NumberParser 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,19 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        Delimiter delimiter = DelimiterExtractor.getDelimiter(input);
-        System.out.println(delimiter);
+        DelimiterExtractor extractor = new DelimiterExtractor(input);
+        System.out.println(extractor.getDelimiter());
+        NumberParser numberParser = new NumberParser(extractor.getRemaining(), extractor.getDelimiter());
+        List<Short> numbers = numberParser.getNumbers();
+        System.out.println(numbers);
         Integer result = 0;
         System.out.printf("결과: %s", result);
-        // Parser -> 숫자들
-        // Calculator -> 결과
-        // 결과를 담아서 "결과 : "라고 입력하기
     }
 }
---
+++ src/main/java/calculator/NumberParser.java
@@ -0,0 +1,39 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NumberParser {
+    private final List<Delimiter> defaultDelimiters = List.of(new Delimiter(","), new Delimiter(";"));
+    private final ArrayList<Short> numbers;
+
+    public NumberParser(String target, Delimiter customDelimiter) {
+        String regex = makeRegexFromDelimiter(customDelimiter);
+        this.numbers = parse(target, regex);
+    }
+
+    private ArrayList<Short> parse(String target, String regex) {
+        ArrayList<Short> result = new ArrayList<>();
+        System.out.println(Arrays.toString(target.split(regex)));
+        for (String s: target.split(regex)) {
+            result.add(Short.valueOf(s));
+        }
+        return result;
+    };
+
+    private String makeRegexFromDelimiter(Delimiter customDelimiter) {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        for (Delimiter delimiter: defaultDelimiters) {
+            result.append(delimiter);
+        }
+        result.append(customDelimiter);
+        result.append("]");
+        return result.toString();
+    }
+
+    public ArrayList<Short> getNumbers() {
+        return numbers;
+    }
+}
